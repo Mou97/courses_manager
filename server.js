212236386dc541669fd2eb7af4296b2cb1e6da79
Mou97 <m.taguiouine@esi-sba.dz>
2020-09-25 22:13:16 +0100

    set up dotenv, mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 
+require('dotenv').config()
+
 const app = express()
+
+// connect to the db 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => console.log(err))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
